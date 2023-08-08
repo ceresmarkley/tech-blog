6099b73aae4881a66e1ffe2c24e53a5a0d92d93c
markley man <129554518+ceresmarkley@users.noreply.github.com>
2023-08-08 10:37:13 -0400

    updated server file with const sess which will store a users cookie session. also added partial handlebar files for comments and post info based off user and post title.

--- server.js
+++ server.js
@@ -4,21 +4,34 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
 // Create the Handlebars.js engine object with custom helper functions
 const hbs = exphbs.create({ helpers });
+const session = require('express-session');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+    secret: 'Super secret secret',
+    cookie: { maxAge: 36000 },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
 
 // Inform Express.js which template engine we're using
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
