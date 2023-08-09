db43394659a7c5d3c5785c1092193ddcf2edd57f
markley man <129554518+ceresmarkley@users.noreply.github.com>
2023-08-09 15:39:02 -0400

    created withAuth and helpers functions to require authorization when called, and using helpers for date format.

--- public/javascript/delete-post.js
+++ public/javascript/delete-post.js
@@ -5,7 +5,7 @@ async function deleteFormHandler(event) {
         window.location.toString().split('/').length - 1
     ];
 
-    const response = await fetch('/api/comments', {
+    const response = await fetch(`/api/posts/${id}`, {
             method: 'DELETE',
             body: JSON.stringify({
                 post_id: id
