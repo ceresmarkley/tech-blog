40369bedee4ba45f715335bee11b67e53062b0fc
markley man <129554518+ceresmarkley@users.noreply.github.com>
2023-08-12 21:52:50 -0400

    updated models and index files to be inline with controller file updates.

--- models/index.js
+++ models/index.js
@@ -1,32 +1,36 @@
-const User = require('./user');
-const Post = require('./post');
 const Comment = require('./comment');
+const Post = require('./post');
+const User = require('./user');
 
 User.hasMany(Post, {
     foreignKey: 'user_id'
 });
+
 Post.belongsTo(User, {
     foreignKey: 'user_id',
-    onDelete: "cascade"
-});
-
-Comment.belongsTo(User, {
-    foreignKey: 'user_id',
-    onDelete: "cascade"
-});
-
-Comment.belongsTo(Post, {
-    foreignKey: 'post_id',
-    onDelete: "cascade"
+    onDelete: "CASCADE"
 });
 
 User.hasMany(Comment, {
     foreignKey: 'user_id',
-    onDelete: "cascade"
+    onDelete: "CASCADE"
+});
+
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: "CASCADE"
 });
 
 Post.hasMany(Comment, {
     foreignKey: 'post_id',
-    onDelete: "cascade"
+    onDelete: "CASCADE"
 })
-module.exports = { User, Post, Comment };
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: "CASCADE"
+});
+
+
+module.exports = { Comment, User, Post };
--- models/user.js
+++ models/user.js
@@ -2,71 +2,58 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
-// create our User model
+
 class User extends Model {
-    // set up method to run on instance data (per user) to check password 
-    checkPassword(loginPW) {
-        return bcrypt.compareSync(loginPW, this.password);
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
     }
 }
 
-// define table columns and configuration
 User.init(
-  {
-    // define an id column
-    id: {
-        // use the special Sequelize DataTypes object provide what type of data it is 
-        type: DataTypes.INTEGER,
-        // equivalent of SQL "NOT NULL"
-        allowNull: false,
-        // instruct that this is the Primary Key
-        primaryKey: true,
-        // turn on auto increment
-        autoIncrement: true
-    },
-    // define a username column
-    username: {
-        type: DataTypes.STRING,
-        allowNull:false,
-        unique: true
-    },
-    // define a password column 
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            // this means the password must be at least four characters long
-            len: [4]
-        }
-    }
-  },
-  {
-    hooks: {
-        // set up beforeCreate lifecycle "hook" functionality
-        async beforeCreate(newUserData) {
-            newUserData.password = await bcrypt.hash(newUserData.password, 10);
-            return newUserData;
-            
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
         },
-        // set up beforeUpdate lifecycle "hook" functionality
-        async beforeUpdate(updatedUserData) {
-            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-            return updatedUserData;
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
         }
     },
-    // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
-
-    // pass in our imported sequelize connection (the direct connection to our database)
-    sequelize,
-    // don't automatically create createdAt/updatedAt timestamp fields
-    timestamps: false,
-    // don't pluralize name of database table
-    freezeTableName: true,
-    // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
-    underscored: true,
-    // make it so our model name stays lowercase in the database
-    modelName: 'user'
-  }
+    {
+        hooks: {
+            async beforeCreate(newUser) {
+                newUser.password = await bcrypt.hash(newUser.password, 12);
+                return newUser;
+            },
+            // async beforeUpdate(updateUser) {
+            //     updateUser.password = await bcrypt.hash(updateUser.password, 12);
+            //     return updateUser;
+            // }
+        },
+        sequelize,
+        timestamps: false,
+        underscored: true,
+        freezeTableName: true,
+        modelName: 'user',
+    }
 );
 
 module.exports = User;
